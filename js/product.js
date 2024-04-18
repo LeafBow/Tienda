3dfbf73d37ba435f056b0865a0e44380f5498e78
Leafbow <mbazandelao@gmail.com>
2024-04-17 20:30:28 -0500

    activadad 3

--- js/product.js
+++ js/product.js
@@ -15,28 +15,57 @@
 
 // Actividad Creacion prodcuto 2
 
-class Product
+// class Product
+// {
+//     constructor(id, title, price, stock, images, onsale)
+//     {
+//         this.id = id;
+//         this.title = title;
+//         this.price = price;
+//         this.stock = stock;
+//         this.images = images;
+//         this.onsale = onsale;
+//     }
+
+// }
+// const prod1 = new Product();
+// const prod2 = new Product('id001','agua',1.5);
+// const prod3 = new Product('id002','pan',1,4,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
+
+// console.log(prod1);
+// console.log(prod2);
+// console.log(prod3);
+
+// console.log("Nombre producto 2: \n"+prod2.title);
+// console.log('Esta en venta del producto 3? \n'+prod3.onsale)
+
+
+// Actividad Set & Get
+
+class Product 
 {
-    constructor(id, title, price, stock, images, onsale)
+    constructor(id, title, price, stock, images, onsale,supplier)
     {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.stock = stock;
-        this.images = images;
-        this.onsale = onsale;
+        this._id = id;
+        this._title = title;
+        this._price = price;
+        this._stock = stock;
+        this._images = images;
+        this._onsale = onsale;
+        this._supplier = supplier;
     }
-
+    get getSupplier()
+    {
+        return this._supplier;
+    }
+    set setSupplier(newName)
+    {
+        this._supplier = newName;
+    }
 }
-const prod1 = new Product();
-const prod2 = new Product('id001','agua',1.5);
-const prod3 = new Product('id002','pan',1,4,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
 
-console.log(prod1);
-console.log(prod2);
-console.log(prod3);
+let prod4 = new Product('id003','gaseosa', 2, 5, "#", true,"Luis.SAC");
 
-console.log("Nombre producto 2: \n"+prod2.title);
-console.log('Esta en venta del producto 3? \n'+prod3.onsale)
-
-
+console.log(prod4.getSupplier);
+prod4.setSupplier = "PedroSAC"
+console.log(prod4.getSupplier);
