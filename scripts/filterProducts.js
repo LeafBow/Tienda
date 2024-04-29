3ad0f5a169f3022279ebd7a035814f3fa6d8726b
Leafbow <mbazandelao@gmail.com>
2024-04-29 16:49:02 -0500

    29-4.1/productFilter_1

--- scripts/filterProducts.js
+++ scripts/filterProducts.js
@@ -1,7 +1,27 @@
-function captureText()
-{
-    console.log("capturado")
-}
-let searchSelector = document.querySelector("#search");
-searchSelector.addEventListener("keyup",event =>captureText(event));
+// function captureText()
+// {
+//     console.log("capturado")
+// }
+// let searchSelector = document.querySelector("#search");
+// searchSelector.addEventListener("keyup",event =>captureText(event));
 
+let searchSelector = document.querySelector("#search");
+let busquedad = "";
+const captureText = (event) => {
+  busquedad = event.target.value;
+  console.log(busquedad);
+};
+
+searchSelector.addEventListener("keyup", (event) => {
+  captureText(event);
+});
+
+// --------Captura de los datos del evento----------------
+// let searchSelector = document.querySelector("#search");
+// const captureText = (eventData) => {
+//   console.log(eventData);
+// };
+
+// searchSelector.addEventListener("keyup", (event) => {
+//   captureText(event);
+// });
--- scripts/product.js
+++ scripts/product.js
@@ -3,12 +3,11 @@
 // const producto1 =
 // {
 //     nombre: "celular",
-//      precio: "1200", 
+//      precio: "1200",
 //      stock: 2,
 // };
 // producto1.id = 'id123';
 // producto1.foto = "https://i.postimg.cc/Jn2C5W84/galaxy1.webp"
-
 
 // console.log(producto1);
 // console.log(producto1.nombre);
@@ -39,10 +38,9 @@
 // console.log("Nombre producto 2: \n"+prod2.title);
 // console.log('Esta en venta del producto 3? \n'+prod3.onsale)
 
-
 // Actividad Set & Get
 
-// class Product 
+// class Product
 // {
 //     constructor(id, title, price, stock, images, onsale,supplier)
 //     {
@@ -72,45 +70,43 @@
 
 // Actividad Venda de productos
 
-class Product
-{
-    constructor(id, title, price, stock, images, onsale){
-        this._id = id;
-        this._title = title;
-        this._price = price;
-        this._stock = stock;
-        this._images = images;
-        this._onsale = onsale;
-    }
+// class Product
+// {
+//     constructor(id, title, price, stock, images, onsale){
+//         this._id = id;
+//         this._title = title;
+//         this._price = price;
+//         this._stock = stock;
+//         this._images = images;
+//         this._onsale = onsale;
+//     }
 
-    sellUnits(units)
-    {
-        if(this._stock>=units)
-        {
-            this._stock -= units;
-        }
-        else
-        {
-            console.log("No hay suficiente stock")
-        }
-        
-    }
+//     sellUnits(units)
+//     {
+//         if(this._stock>=units)
+//         {
+//             this._stock -= units;
+//         }
+//         else
+//         {
+//             console.log("No hay suficiente stock")
+//         }
 
-}
-const prod1 = new Product('id001');
-const prod2 = new Product('id002','agua',1.5);
-const prod3 = new Product('id003','pan',1,4,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
-const prod4 = new Product('id004','gaseosa', 2, 5, "#", true,"Luis.SAC");
-const prod5 = new Product('id005','pan',1,12,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
-const prod6 = new Product('id006','galleta',2,5,0,true);
+//     }
 
+// }
+// const prod1 = new Product('id001');
+// const prod2 = new Product('id002','agua',1.5);
+// const prod3 = new Product('id003','pan',1,4,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
+// const prod4 = new Product('id004','gaseosa', 2, 5, "#", true,"Luis.SAC");
+// const prod5 = new Product('id005','pan',1,12,"https://th.bing.com/th/id/OIP.d-zNWc9hB05nn0sO6_42EgHaHa?rs=1&pid=ImgDetMain",true);
+// const prod6 = new Product('id006','galleta',2,5,0,true);
 
+// const products = [prod1,prod2,prod3,prod4];
 
-const products = [prod1,prod2,prod3,prod4];
-
-console.log(products);
-console.log(products[1]);
-console.log(products[products.length-1])
-products.unshift(prod5);
-products.push(prod6);
-console.log(products);
+// console.log(products);
+// console.log(products[1]);
+// console.log(products[products.length-1])
+// products.unshift(prod5);
+// products.push(prod6);
+// console.log(products);
